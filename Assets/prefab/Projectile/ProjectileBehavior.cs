0721b598b4a048c583f9b70cdbe8009bac3ffdee
zathp <56136084+zathp@users.noreply.github.com>
2023-06-11 19:28:30 -0700

    Projectile physics Improvements

--- Assets/prefab/Projectile/ProjectileBehavior.cs
+++ Assets/prefab/Projectile/ProjectileBehavior.cs
@@ -6,7 +6,7 @@ using Mirror;
 
 public class ProjectileBehavior : NetworkBehaviour
 {
-    [SerializeField] float ProjectileOffset = 5.0f;
+    [SerializeField] float ProjectileOffset = 0;
 
     [HideInInspector] public PlanetManager planetMan;
 
@@ -17,6 +17,7 @@ public class ProjectileBehavior : NetworkBehaviour
 
     //scriptable objects
     private ProjectileManager ProjectileSettings;
+    private Vector2 lastContactPoint = new Vector2(0,0);
 
     public void Init(ProjectileManager ProjectileSettingsIn, Vector3 initialPosition, Vector2 initialVelocity, float angle) {
         planetMan = FindFirstObjectByType<PlanetManager>();
@@ -34,9 +35,6 @@ public class ProjectileBehavior : NetworkBehaviour
             Destroy(gameObject);
         rb.rotation = 180.0f / PI * Atan2(rb.velocity.y, rb.velocity.x) - 180;
         timeAlive += Time.deltaTime;
-
-        
-
     }
 
     void FixedUpdate()
@@ -50,9 +48,36 @@ public class ProjectileBehavior : NetworkBehaviour
     private void OnCollisionEnter2D(Collision2D collision) {
         GameObject other = collision.gameObject;
         if(other.CompareTag("Player")) {
+            Rigidbody2D otherBody = other.GetComponent<Rigidbody2D>();
+            Vector2 otherVel = otherBody.velocity;
+            Vector2 relVel = (rb.velocity-otherVel);
             Player po = other.GetComponent<Player>();
-            po.changeHealth(po.health - 5);
+            po.changeHealth(po.health - ProjectileSettings.projectileBaseDamage * Pow(relVel.magnitude,2)/Pow(ProjectileSettings.projectileBaseVel,2));
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
+        
+        else {
+            Rigidbody2D otherBody = other.GetComponent<Rigidbody2D>();
+            Vector2 otherVel;
+            if(otherBody != null)
+                otherVel = otherBody.velocity;
+            else
+                otherVel = new Vector2(0,0);
+            if((rb.velocity-otherVel).magnitude * 100 /(ProjectileSettings.projectileElacticity + 1) > ProjectileSettings.projectileBaseVel) {
+                ContactPoint2D[] contact = {new ContactPoint2D()};
+                int numpoints = collision.GetContacts(contact);
+                
+                if((lastContactPoint-contact[0].point).magnitude>(rb.velocity-otherVel).magnitude/10) {
+                    Vector2 normRelVel = contact[0].normal*Vector2.Dot((rb.velocity-otherVel),contact[0].normal)/Vector2.Dot(contact[0].normal,contact[0].normal);
+                    Vector2 tanRelVel = (rb.velocity-otherVel)-normRelVel;
+                    rb.velocity = otherVel +
+                        (normRelVel * (-ProjectileSettings.projectileElacticity) + tanRelVel);
+                    lastContactPoint = contact[0].point;
+                }
+            }
+            else
+                Destroy(gameObject);
+        } 
+
     }
 }
